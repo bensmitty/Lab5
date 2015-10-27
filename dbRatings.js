1d4760bc88d68c3ed5d2cce403168345b94104d2
bensmitty <ben_smitty@hotmail.com>
2015-10-27 00:28:44 -0400

    avg working - limits to 2 decimal places

--- dbRatings.js
+++ dbRatings.js
@@ -11,7 +11,7 @@ var getRating = function getRating(product_id, callback) {
     db.pool.getConnection(function (err, connection) {
         // Use the connection
         //connection.query('SELECT * FROM PRODUCTS WHERE ?', get, function (err, results) {
-        connection.query('SELECT AVG(rating) FROM csc443.ratings WHERE ?', get, function (err, results) {
+        connection.query('SELECT CAST(AVG(rating) AS DECIMAL(3,2)) FROM csc443.ratings WHERE ?', get, function (err, results) {
             if (!err) {
                 if (results[0] != null) {
                     callback(null, results);
